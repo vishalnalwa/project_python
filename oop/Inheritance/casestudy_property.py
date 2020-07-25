1b475243e8d1796e9cdd78fe772270910ec1b7d1
vishalnalwa <vishalnalwa@hotmail.com>
2020-07-25 15:12:34 +1000

    added agent class

--- oop/Inheritance/casestudy_property.py
+++ oop/Inheritance/casestudy_property.py
@@ -144,9 +144,34 @@ class ApartmentRental(Rental,Apartment):
         return init
     prompt_init = staticmethod(prompt_init)
 
-class ApartmentRental(Purchase,Apartment):
+
+class ApartmentPurchase(Purchase,Apartment):
     def prompt_init():
         init = Apartment.prompt_init()
         init.update(Purchase.prompt_init())
         return init
     prompt_init = staticmethod(prompt_init)
+
+class Agent:
+
+    type_map = {
+        ("house","rental"):HouseRental,
+        ("house","purchase"):HousePurchase,
+        ("apartment","rental"):ApartmentRental,
+        ("apartment","purchase"):ApartmentPurchase
+    }
+
+    def __init__(self):
+        self.property_list = []
+    
+    def display_poperties(self):
+        for property in self.property_list:
+            property.display()
+    
+    def add_property(self):
+        property_type = get_valid_input("what type of property are you after?",("house","apartment")).lower()
+        payment_type = get_valid_input("Do you want to purchase of rent?",("purchase","rental")).lower()
+
+        PropertyClass = self.type_map[(property_type,payment_type)]
+        init_args = PropertyClass.prompt_init()
+        self.property_list.append(PropertyClass(**init_args))
